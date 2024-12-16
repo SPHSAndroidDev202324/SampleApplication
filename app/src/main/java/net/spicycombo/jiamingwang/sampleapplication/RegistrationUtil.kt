cbef2ec4a334996a10295847e129ec2fa9593cb4
per2 <jiamingwang+per2@spicycombo.net>
2024-12-16 08:30:49 -0800

    ok

--- app/src/main/java/net/spicycombo/jiamingwang/sampleapplication/RegistrationUtil.kt
+++ app/src/main/java/net/spicycombo/jiamingwang/sampleapplication/RegistrationUtil.kt
@@ -8,22 +8,34 @@ class RegistrationUtil {
         // you can use listOf<type>() instead of making the list & adding individual
         // List<String> blah...
 
+        // Can check against a database for if the username already exists
+        fun isExistingUser(username : String) : Boolean {
+            return existingUsers.contains(username)
+        }
+
         // isn't empty
         // not already taken
         // minimum number of characters is 3
 
         fun validateUsername(username: String) : Boolean {
+            if (isExistingUser(username)) return false
+            // do other things here too!
             return false
             // TODO: Implement
         }
 
         // Not empty
         // Minimum 16 characters, maximum of 128 characters
-        // At least contains numbers,
+        // At least contains 3 numerical digit,
         //  lower and capitalized characters,
         //  special characters !@#$%*
         //
         fun validatePassword(password: String, confirm: String) : Boolean {
+            if (password.isNullOrEmpty() || password.length < 16 || password.length > 128) return false
+            if (password.count { it.isDigit() } < 3) return false
+            if (password.count { it.isUpperCase() } < 1) return false
+            if (password.count { it.isLowerCase() } < 1) return false
+            // if (password.count { it == '!' })
             return false
             // TODO: Implement
         }
